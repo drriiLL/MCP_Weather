8ef366421d5b0b10492d9531258104be44a4840a
drriiLL <160042762+drriiLL@users.noreply.github.com>
2026-02-17 17:51:20 +0300

    Добавленны новые инструменты

--- server_MCP.py
+++ server_MCP.py
@@ -4,8 +4,8 @@ import asyncio
 from mcp.server.fastmcp import FastMCP
 from deep_translator import GoogleTranslator
 import typing 
-
-
+from docx import Document
+import os
 
 USER_AGENT = "weather-app/1.0"
 
@@ -29,6 +29,38 @@ async def make_requests(rus_city: str) -> str:
         except Exception as e:
             return f"Произошла ошибка: {e}"
 
+def get_doc_text(name: str) -> list:
+    doc = Document(os.path.join("C:\\VScode\\Python\\Ai\\code", name))
+    text = []
+    for paragraph in doc.paragraphs:
+        if paragraph.text.strip():
+            text.append(paragraph.text)
+    for table in doc.tables:
+        for row in table.rows:
+            for cell in row.cells:
+                if cell.text.strip():
+                    text.append(cell.text)
+    return text
+
+@mcp.tool()
+async def create_doc(name: str, text: str) -> str:
+    """
+    Tool for create_doc
+    
+    Args:
+        name: Name of file
+        text: Text which need paste in document
+    """
+
+    doc = Document()
+    try:
+        doc.add_paragraph(text)
+        doc.save(name)
+        return "Готово"
+    except Exception as e:
+        return f"Произошла ошибка {e}"
+
+
 
 @mcp.tool()
 async def get_weather(city: str) -> str:
@@ -39,6 +71,18 @@ async def get_weather(city: str) -> str:
     """
     result = await make_requests(city)
     return result or "Unable to fetch weather."
+
+
+
+@mcp.tool()
+async def read_doc(name: str) -> str:
+    """Read word document and return its text content.
+    
+    Args:
+        name: Name of word file with extension, e.g. 'document.docx'
+    """
+    result = get_doc_text(name)
+    return "\n".join(result)
 
 
 def main():
